8599f3711cd0ede61e8dfc0db6e5ffd0fb0e172b
Lachlan Cresswell <lachy93@gmail.com>
2022-01-07 00:25:35 +0000

    Move html to external ts file

---
+++ client/src/html.ts
@@ -0,0 +1,169 @@
+export const pageBasic = () => '<div id="basic-details"> <div class="details-phase" id="details-l1">' +
+    '                <div class="details-voltage">' +
+    '                    <div id="l1-voltage" class="details-value">' +
+    '                        247' +
+    '                    </div>' +
+    '                    <div class="details-denominator">' +
+    '                        V' +
+    '                    </div>' +
+    '                </div>' +
+    '                <div class="details-amperage">' +
+    '                    <div id="l1-amperage-round" class="details-value">' +
+    '                        0.0' +
+    '                    </div>' +
+    '                    <div class="details-denominator">' +
+    '                        A' +
+    '                    </div>' +
+    '                </div>' +
+    '            </div>' +
+    '            <div class="details-phase" id="details-l2">' +
+    '                <div class="details-voltage">' +
+    '                    <div id="l2-voltage" class="details-value">' +
+    '                        247' +
+    '                    </div>' +
+    '                    <div class="details-denominator">' +
+    '                        V' +
+    '                    </div>' +
+    '                </div>' +
+    '                <div class="details-amperage">' +
+    '                    <div id="l2-amperage-round" class="details-value">' +
+    '                        0.0' +
+    '                    </div>' +
+    '                    <div class="details-denominator">' +
+    '                        A' +
+    '                    </div>' +
+    '                </div>' +
+    '            </div>' +
+    '            <div class="details-phase" id="details-l3">' +
+    '                <div class="details-voltage">' +
+    '                    <div id="l3-voltage" class=" details-value">' +
+    '                        247' +
+    '                    </div>' +
+    '                    <div class="details-denominator">' +
+    '                        V' +
+    '                    </div>' +
+    '                </div>' +
+    '                <div class="details-amperage">' +
+    '                    <div id="l3-amperage-round" class="details-value">' +
+    '                        0.0' +
+    '                    </div>' +
+    '                    <div class="details-denominator">' +
+    '                        A' +
+    '                    </div>' +
+    '                </div>' +
+    '            </div></div>';
+
+export const pagePhase = (phase: number) =>
+    `<div id="details">
+                    <div class="phase-info" id="details-l${phase}">
+                        <div id="details-l1-voltage">
+                            <div id="l${phase}-voltage" class="details-value details-more-70">
+                                247
+                            </div>
+                            <div class="details-denominator details-more-30">
+                                V
+                            </div>
+                        </div>
+                        <div id="details-l1-amperage">
+                            <div id="l${phase}-amperage" class="details-value details-more-70">
+                                0.0
+                            </div>
+                            <div class="details-denominator details-more-30">
+                                A
+                            </div>
+                        </div>
+                    </div>
+                    <div id="details-minor">
+                        <div class="pf" id="pf">
+                            <div id="power-factor" class="details-value details-more-70">
+                                0.0
+                            </div>
+                            <div class="details-denominator details-more-30">
+                                PF
+                            </div>
+                        </div>
+                        <div class="kva" id="kva">
+                            <div id="apparent-power" class="details-value details-more-70">
+                                0.0
+                            </div>
+                            <div class="details-denominator details-more-30">
+                                kVA
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+
+
+export const pageAdv = () =>
+    `<div id="container"><div id="subcontainer-1">
+                    <div id="adv-col-1">
+                        <div id="l1-voltage" class="value-container l1">
+                            246
+                        </div>
+                        <div id="l2-voltage" class="value-container l2">
+                            245
+                        </div>
+                        <div id="l3-voltage" class="value-container l3">
+                            246
+                        </div>
+                        <div id="power-factor" class="value-container pf">
+                            0.0
+                        </div>
+                        <div id="apparent-power" class="value-container kva">
+                            0.0
+                        </div>
+                    </div>
+                    <div id="adv-col-2">
+                        <div class="denomination-container l1">
+                            V
+                        </div>
+                        <div class="denomination-container l2">
+                            V
+                        </div>
+                        <div class="denomination-container l3">
+                            V
+                        </div>
+                        <div class="denomination-container pf">
+                            pf
+                        </div>
+                        <div class="denomination-container kva">
+                            kVA
+                        </div>
+                    </div>
+                </div>
+                <div id="subcontainer-2">
+                    <div id="adv-col-3">
+                        <div id="l1-amperage" class="value-container l1">
+                            0.0
+                        </div>
+                        <div id="l2-amperage" class="value-container l2">
+                            0.0
+                        </div>
+                        <div id="l3-amperage" class="value-container l3">
+                            0.0
+                        </div>
+                        <div class="value-container blank">
+                            EMP
+                        </div>
+                        <div id="grid-freq" class="value-container freq">
+                            50
+                        </div>
+                    </div>
+                    <div id="adv-col-4">
+                        <div class="denomination-container l1">
+                            A
+                        </div>
+                        <div class="denomination-container l2">
+                            A
+                        </div>
+                        <div class="denomination-container l3">
+                            A
+                        </div>
+                        <div class="denomination-container blank">
+                            TY
+                        </div>
+                        <div class="denomination-container freq">
+                            Hz
+                        </div>
+                    </div>
+                </div></div>`;
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,5 @@
 import * as influx from 'influx';
+import * as HTML from './html'
 
 // GLOBALS
 const dbs: { influx: influx.InfluxDB, address: string, local: boolean }[] = [];
@@ -7,176 +8,6 @@ let curDevice = 0;
 const devMenu = document.getElementById("device-menu") as HTMLDivElement
 
 // CONSTS
-
-const basicHTML = () => '<div id="basic-details"> <div class="details-phase" id="details-l1">' +
-    '                <div class="details-voltage">' +
-    '                    <div id="l1-voltage" class="details-value">' +
-    '                        247' +
-    '                    </div>' +
-    '                    <div class="details-denominator">' +
-    '                        V' +
-    '                    </div>' +
-    '                </div>' +
-    '                <div class="details-amperage">' +
-    '                    <div id="l1-amperage-round" class="details-value">' +
-    '                        0.0' +
-    '                    </div>' +
-    '                    <div class="details-denominator">' +
-    '                        A' +
-    '                    </div>' +
-    '                </div>' +
-    '            </div>' +
-    '            <div class="details-phase" id="details-l2">' +
-    '                <div class="details-voltage">' +
-    '                    <div id="l2-voltage" class="details-value">' +
-    '                        247' +
-    '                    </div>' +
-    '                    <div class="details-denominator">' +
-    '                        V' +
-    '                    </div>' +
-    '                </div>' +
-    '                <div class="details-amperage">' +
-    '                    <div id="l2-amperage-round" class="details-value">' +
-    '                        0.0' +
-    '                    </div>' +
-    '                    <div class="details-denominator">' +
-    '                        A' +
-    '                    </div>' +
-    '                </div>' +
-    '            </div>' +
-    '            <div class="details-phase" id="details-l3">' +
-    '                <div class="details-voltage">' +
-    '                    <div id="l3-voltage" class=" details-value">' +
-    '                        247' +
-    '                    </div>' +
-    '                    <div class="details-denominator">' +
-    '                        V' +
-    '                    </div>' +
-    '                </div>' +
-    '                <div class="details-amperage">' +
-    '                    <div id="l3-amperage-round" class="details-value">' +
-    '                        0.0' +
-    '                    </div>' +
-    '                    <div class="details-denominator">' +
-    '                        A' +
-    '                    </div>' +
-    '                </div>' +
-    '            </div></div>';
-
-const updatePhase = (phase: number) =>
-    `<div id="details">
-                    <div class="phase-info" id="details-l${phase}">
-                        <div id="details-l1-voltage">
-                            <div id="l${phase}-voltage" class="details-value details-more-70">
-                                247
-                            </div>
-                            <div class="details-denominator details-more-30">
-                                V
-                            </div>
-                        </div>
-                        <div id="details-l1-amperage">
-                            <div id="l${phase}-amperage" class="details-value details-more-70">
-                                0.0
-                            </div>
-                            <div class="details-denominator details-more-30">
-                                A
-                            </div>
-                        </div>
-                    </div>
-                    <div id="details-minor">
-                        <div class="pf" id="pf">
-                            <div id="power-factor" class="details-value details-more-70">
-                                0.0
-                            </div>
-                            <div class="details-denominator details-more-30">
-                                PF
-                            </div>
-                        </div>
-                        <div class="kva" id="kva">
-                            <div id="apparent-power" class="details-value details-more-70">
-                                0.0
-                            </div>
-                            <div class="details-denominator details-more-30">
-                                kVA
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
-
-
-const updateAdv = () =>
-    `<div id="container"><div id="subcontainer-1">
-                    <div id="adv-col-1">
-                        <div id="l1-voltage" class="value-container l1">
-                            246
-                        </div>
-                        <div id="l2-voltage" class="value-container l2">
-                            245
-                        </div>
-                        <div id="l3-voltage" class="value-container l3">
-                            246
-                        </div>
-                        <div id="power-factor" class="value-container pf">
-                            0.0
-                        </div>
-                        <div id="apparent-power" class="value-container kva">
-                            0.0
-                        </div>
-                    </div>
-                    <div id="adv-col-2">
-                        <div class="denomination-container l1">
-                            V
-                        </div>
-                        <div class="denomination-container l2">
-                            V
-                        </div>
-                        <div class="denomination-container l3">
-                            V
-                        </div>
-                        <div class="denomination-container pf">
-                            pf
-                        </div>
-                        <div class="denomination-container kva">
-                            kVA
-                        </div>
-                    </div>
-                </div>
-                <div id="subcontainer-2">
-                    <div id="adv-col-3">
-                        <div id="l1-amperage" class="value-container l1">
-                            0.0
-                        </div>
-                        <div id="l2-amperage" class="value-container l2">
-                            0.0
-                        </div>
-                        <div id="l3-amperage" class="value-container l3">
-                            0.0
-                        </div>
-                        <div class="value-container blank">
-                            EMP
-                        </div>
-                        <div id="grid-freq" class="value-container freq">
-                            50
-                        </div>
-                    </div>
-                    <div id="adv-col-4">
-                        <div class="denomination-container l1">
-                            A
-                        </div>
-                        <div class="denomination-container l2">
-                            A
-                        </div>
-                        <div class="denomination-container l3">
-                            A
-                        </div>
-                        <div class="denomination-container blank">
-                            TY
-                        </div>
-                        <div class="denomination-container freq">
-                            Hz
-                        </div>
-                    </div>
-                </div></div>`;
 
 // FUNCTIONS
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
@@ -333,9 +164,9 @@ const buttonHandler = (button: string, contents: any) => {
     }
 }
 
-buttonHandler("button-basic", basicHTML())
-buttons["button-basic"].onclick = () => buttonHandler("button-basic", basicHTML())
-buttons["button-l1"].onclick = () => buttonHandler("button-l1", updatePhase(1))
-buttons["button-l2"].onclick = () => buttonHandler("button-l2", updatePhase(2))
-buttons["button-l3"].onclick = () => buttonHandler("button-l3", updatePhase(3))
-buttons["button-adv"].onclick = () => buttonHandler("button-adv", updateAdv())
+buttonHandler("button-basic", HTML.pageBasic())
+buttons["button-basic"].onclick = () => buttonHandler("button-basic", HTML.pageBasic())
+buttons["button-l1"].onclick = () => buttonHandler("button-l1", HTML.pagePhase(1))
+buttons["button-l2"].onclick = () => buttonHandler("button-l2", HTML.pagePhase(2))
+buttons["button-l3"].onclick = () => buttonHandler("button-l3", HTML.pagePhase(3))
+buttons["button-adv"].onclick = () => buttonHandler("button-adv", HTML.pageAdv())
