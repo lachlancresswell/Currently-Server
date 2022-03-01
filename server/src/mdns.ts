eb54bef637dd2e09d36032cf1ce3e1f436811108
Lachlan Cresswell <lachy93@gmail.com>
2022-03-01 12:44:10 +0000

    Move mdns updates

--- server/src/mdns.ts
+++ server/src/mdns.ts
@@ -1,4 +1,8 @@
+import makeMdns from 'multicast-dns';
+
 const MDNS_RECORD_TYPE = 'SRV';
+
+const mdns = makeMdns({ loopback: true });
 
 /**
  * Validates given query questions or response answers
@@ -7,3 +11,49 @@ const MDNS_RECORD_TYPE = 'SRV';
  */
 
 export const validatePacket = (packet: { type: string }[]) => packet[0] && packet[0].type === MDNS_RECORD_TYPE;
+
+export const attachResponseHandler = (cb: any) => {
+    mdns.once('response', cb);
+};
+
+export const sendUpdate = (HTTP_MDNS_SERVICE_NAME: string, HTTPS_MDNS_SERVICE_NAME: string, MDNS_DOMAIN: string, HTTP_PORT: number, HTTPS_PORT: number, ips: string[], deviceName: string, MODBUS_GATEWAY_IP: string | undefined) => {
+    const type = MDNS_RECORD_TYPE;
+    const weight = 0;
+    const priority = 10;
+    let answers: any = [
+        {
+            name: HTTP_MDNS_SERVICE_NAME,
+            type,
+            data: {
+                port: HTTP_PORT,
+                weight,
+                priority,
+                target: HTTP_MDNS_SERVICE_NAME + MDNS_DOMAIN
+            }
+        }, {
+            name: HTTPS_MDNS_SERVICE_NAME,
+            type,
+            data: {
+                port: HTTPS_PORT,
+                weight,
+                priority,
+                target: HTTPS_MDNS_SERVICE_NAME + MDNS_DOMAIN
+            }
+        }]
+
+    ips.forEach((ipAddress: string) => {
+        answers.push({
+            name: deviceName + MDNS_DOMAIN,
+            type: 'A',
+            data: ipAddress
+        })
+    });
+
+    answers.push({
+        name: 'modbus' + MDNS_DOMAIN,
+        type: 'A',
+        data: MODBUS_GATEWAY_IP || '0.0.0.0'
+    })
+
+    mdns.respond({ answers })
+}
