652778a762090fe1c0aba8d103be12c9194e8091
Lachlan <lachlancresswell@gmail.com>
2022-09-02 16:19:25 +1000

    Add light/dark theme switching

--- client-react/distro-ui/src/Routes/PageDisplay.tsx
+++ client-react/distro-ui/src/Routes/PageDisplay.tsx
@@ -6,16 +6,19 @@ import Brightness4Icon from '@mui/icons-material/Brightness4';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 export default function PageDisplay({ }: {}) {
-    let startPeriod = JSON.parse(window.localStorage.getItem('period') || '') || '30s';
+    let startPeriod = JSON.parse(window.localStorage.getItem('period') || '"30s"');
     let startBrightness = JSON.parse(window.localStorage.getItem('brightness') || '100');
+    let startTheme = JSON.parse(window.localStorage.getItem('theme') || 'dark');
     const [period, setPeriod] = useState(startPeriod);
     const [brightness, setBrightness] = useState(startBrightness);
+    const [theme, setTheme] = useState(startTheme);
+    const body = document.querySelector('body') as any;
 
     useEffect(() => {
-        const storedPeriod = JSON.parse(window.localStorage.getItem('period') || '30s')
+        const storedPeriod = JSON.parse(window.localStorage.getItem('period') || '"30s"')
         setPeriod(storedPeriod);
 
-        const storedBrightness = JSON.parse(window.localStorage.getItem('brightness') || '100')
+        const storedBrightness = JSON.parse(window.localStorage.getItem('brightness') || '"100"')
         setBrightness(storedBrightness || '100');
     }, []);
 
@@ -27,12 +30,22 @@ export default function PageDisplay({ }: {}) {
         localStorage.setItem('brightness', JSON.stringify(brightness));
     }, [brightness]);
 
+    useEffect(() => {
+        localStorage.setItem('theme', JSON.stringify(theme));
+        body!.dataset['theme'] = theme;
+    }, [theme]);
+
+    const toggleTheme = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) setTheme('dark')
+        else setTheme('light');
+    }
+
     return (<div className='pageDisplay'>
         <div className="pageRow2">
             <Brightness4Icon className="pageCol1 displayIcon" />
             <span className="pageCol4">
                 <label className="switch">
-                    <input type="checkbox" />
+                    <input type="checkbox" defaultChecked={body!.dataset['theme'] === 'dark'} onChange={toggleTheme} />
                     <span className="slider round"></span>
                 </label>
             </span>
