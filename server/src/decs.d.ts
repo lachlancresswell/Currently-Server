5ab5184637f2ab3f76ef2238239718c3867c9824
Lachlan <lachlancresswell@gmail.com>
2023-04-15 03:58:54 +1000

    refactor (server): remove old server code

--- server/src/decs.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'set-ip-address'
--- server/src/events.ts
+++
@@ -1,3 +0,0 @@
-export const DEVICE_NAME_UPDATE = 'device-name-update';
-export const NEIGHBOUR_DISCOVERED = 'neighbour-discovered';
-export const CONFIG_UPDATE = 'config-'
