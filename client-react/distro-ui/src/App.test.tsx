7d79e4f468b6f1be6f1b282485f3c8e651e10fa5
Lachlan <lachlancresswell@gmail.com>
2023-04-19 10:24:02 +1000

    refactor (client-react): remove previous client

--- client-react/distro-ui/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/l1/i);
-  expect(linkElement).toBeInTheDocument();
-});
