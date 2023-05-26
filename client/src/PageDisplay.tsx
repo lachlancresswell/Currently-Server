a3782e3205457842500e4952ec129a0ee9ef0aec
Lachlan <lachlancresswell@gmail.com>
2023-05-27 07:52:42 +1000

    feat (client): further display page styles and functions

--- client/src/PageDisplay.tsx
+++ client/src/PageDisplay.tsx
@@ -1,10 +1,11 @@
 import { useNeighbourContext } from './neighbourContext';
 import { useNeighbourDataContext } from './neighbourDataContext';
-import { PhaseData, DistroData } from '../../Types';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import './Styles/PageDisplay.css'
+import { ChangeEvent, useState } from 'react';
+import useLocalStorage from 'use-local-storage';
 
 interface PageDisplayProps {
 }
@@ -12,10 +13,22 @@ interface PageDisplayProps {
 export const PageDisplay = ({ }: PageDisplayProps) => {
     const { selectedNeighbour } = useNeighbourContext();
     const { neighbourData } = useNeighbourDataContext();
+    const [sliderValue, setSliderValue] = useState(50);
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+    const isChecked = theme === 'light';
 
     if (!selectedNeighbour) {
         return null;
     }
+
+    const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    };
+
+    const handleSliderChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSliderValue(parseInt(event.target.value));
+    };
 
     return (
         <div className="gridDisplay">
@@ -24,18 +37,18 @@ export const PageDisplay = ({ }: PageDisplayProps) => {
             </div>
             <div className={`span-four-display displaySwitch`}>
                 <label className="switch">
-                    <input type="checkbox" />
+                    <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
                     <span className="slider round">
-                        <span className="switch-on">ON</span>
-                        <span className="switch-off">OFF</span>
+                        <span className="switch-on">{isChecked ? 'ON' : ''}</span>
+                        <span className="switch-off">{!isChecked ? 'OFF' : ''}</span>
                     </span>
                 </label>
             </div>
             <div className={`span-one-display`}>
                 <Brightness4Icon />
             </div>
-            <div className={`span-four-display displaySlider`}>
-                <input type="range" min="0" max="100" value="50" id="slider" />
+            <div className={`span-four-display range-slider`}>
+                <input type="range" min="0" max="100" value={sliderValue} id="slider" onChange={handleSliderChange} />
             </div>
             <div className={`span-one-display`}>
                 <AccessTimeIcon />
@@ -63,45 +76,3 @@ export const PageDisplay = ({ }: PageDisplayProps) => {
         </div>
     );
 };
-
-
-const PhaseRow = ({ phaseIndex, neighbourData }: { phaseIndex: 0 | 1 | 2, neighbourData: DistroData | null }) => {
-    return (
-        <>
-            <div className={`span-six-display ${'l' + (phaseIndex + 1)}`}>
-                <span className="valueBasic">
-                    {(neighbourData?.phases[phaseIndex]!.voltage! > -1 && neighbourData?.phases[phaseIndex]!.voltage)}
-                </span>
-                <span className="unitBasic">
-                    v
-                </span>
-            </div>
-            <div className={`span-four-display ${'l' + (phaseIndex + 1)}`}>
-                <div className='basicAmperage'>
-                    <span className="valueBasicAmperage">
-                        {(neighbourData?.phases[phaseIndex]!.amperage! > -1 ? neighbourData?.phases[phaseIndex]!.amperage : '-')}
-                    </span>
-                    <span className="unitBasic">
-                        a
-                    </span>
-                </div>
-            </div>
-        </>
-    )
-}
-
-const DisplayRow = ({ className, prefix, neighbourData }: { className: string, prefix: string, neighbourData: DistroData | null }) => {
-    return (
-        <>
-            <div className={`span-ten-display ${className}`}>
-                <span className="valueBasic">
-                    {prefix === 'pf' ? neighbourData?.pf
-                        : neighbourData?.kva}
-                </span>
-                <span className="unitBasic">
-                    {prefix}
-                </span>
-            </div>
-        </>
-    )
-}
