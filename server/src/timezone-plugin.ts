40c738b50a082f064ab9ee51b110cd941ea4ee4a
Lachlan <lachlancresswell@gmail.com>
2023-06-01 06:48:44 +1000

    feat (timezone-plugin): update date retrieval methods

--- server/src/timezone-plugin.ts
+++ server/src/timezone-plugin.ts
@@ -1,15 +1,7 @@
 // Influx-plugin.ts
 import { Plugin } from './plugin';
-import { ConfigArray, ConfigVariableMetadata, EphemeralVariableMetaData, timezone } from '../../Types';
+import { DateType, TZOptions, TimeType } from '../../Types';
 import { execSync } from 'child_process';
-
-export interface TZOptions extends ConfigArray {
-    date: EphemeralVariableMetaData<string>;
-    timezone: EphemeralVariableMetaData<timezone>;
-    ntpServers: EphemeralVariableMetaData<string[]>;
-    dateFormat: ConfigVariableMetadata<string>;
-    timeFormat: ConfigVariableMetadata<string>;
-}
 
 class TimeZonePlugin extends Plugin<TZOptions> {
     name = 'TimeZonePlugin';
@@ -17,14 +9,15 @@ class TimeZonePlugin extends Plugin<TZOptions> {
     constructor(serverRouter: any, options: TZOptions) {
         super(serverRouter, options);
 
-        this.setEphemeralVariable(this.configuration.date, this.getDate, this.setDate);
+        this.setEphemeralVariable(this.configuration.date, () => this.getDate(this.configuration.dateFormat.value), this.setDate);
+        this.setEphemeralVariable(this.configuration.time, () => this.getTime(this.configuration.timeFormat.value), this.setTime);
         this.setEphemeralVariable(this.configuration.timezone, this.getTimezone, this.setTimezone);
         this.setEphemeralVariable(this.configuration.ntpServers, this.getNtpServers, this.setNtpServers);
     }
 
     /**
-     * Gets the current device timezone
-     * @returns The current timezone
+     * Gets the current device timezone in GMT format
+     * @returns The current timezone in Country/City format
      */
     getTimezone = () => {
         return execSync(`timedatectl show --property=Timezone`).toString().split('=')[1].trim()
@@ -32,16 +25,17 @@ class TimeZonePlugin extends Plugin<TZOptions> {
 
     /**
      * Sets the device timezone
-     * @param timezone The timezone to set in <country>/<city> format
+     * @param timezone The timezone to set in 'Region/City' format
      * @returns void
      * @throws Error if the timezone is invalid
-     * @throws Error if the timezone is not found
      * @throws Error if the timezone is not configured
      * @example setTimezone('Australia/Melbourne')
      */
     setTimezone = (timezone: string): void => {
         try {
-            execSync(`timedatectl set-timezone ${timezone}`);
+            // Throws if the timezone is invalid
+            execSync(`timedatectl list-timezones | grep ${timezone}`);
+            execSync(`sudo timedatectl set-timezone ${timezone}`);
             console.log(`Configured timezone to ${timezone}`);
         } catch (error) {
             console.error(`Error configuring timezone: ${(error as Error).message}`);
@@ -68,19 +62,88 @@ class TimeZonePlugin extends Plugin<TZOptions> {
     }
 
     /**
-     * Gets the current device date
-     * @returns The current device date
-     * @throws Error if the date is not configured
-     * @example getDate()
-     */
-    getDate(): string {
+ * Gets the current device date
+ * @param {string} format - The desired format for the date ('DMY' or 'MDY')
+ * @returns The current device date in the specified format
+ * @throws Error if the date is not configured
+ * @example getDate('DMY')
+ */
+    getDate(format: DateType = 'dmy'): string {
         const output = execSync('timedatectl').toString();
-        const matches = /Local time: (.+)/.exec(output);
+        const matches = /Local time: \S+ (\S+)/.exec(output);
+
         if (!matches) {
             throw new Error('Unable to parse date from timedatectl output');
         }
-        const dateString = matches[1];
-        return dateString;
+
+        // Parse the date string into a JavaScript Date object
+        const dateParts = matches[1].split('-');
+        const dateObj = new Date(+dateParts[0], +dateParts[1] - 1, +dateParts[2]);
+
+        // Format the date according to the specified format
+        const day = dateObj.getDate();
+        const month = dateObj.getMonth() + 1; // JavaScript months are 0-11
+        const year = dateObj.getFullYear();
+
+        let rtn = ''
+        if (format === 'dmy') {
+            rtn = `${day < 10 ? '0' : ''}${day}/${month < 10 ? '0' : ''}${month}/${year}`;
+        } else if (format === 'mdy') {
+            rtn = `${month < 10 ? '0' : ''}${month}/${day < 10 ? '0' : ''}${day}/${year}`;
+        } else {
+            throw new Error(`Invalid format: ${format}`);
+        }
+
+        return rtn;
+    }
+
+    /**
+     * Gets the current device time
+     * @param {TimeType} timeFormat - The desired format for the time ('24h' or '12h')
+     * @returns The current device time in the specified format
+     * @throws Error if the time is not configured
+     * @example getTime('12h')
+     */
+    getTime(timeFormat: TimeType = '24h'): string {
+        const output = execSync('timedatectl').toString();
+        const matches = /Local time: \S+ \S+ (\S+)/.exec(output);
+
+        if (!matches) {
+            throw new Error('Unable to parse time from timedatectl output');
+        }
+
+        // Parse the time string into a JavaScript Date object
+        const timeParts = matches[1].split(':');
+        const dateObj = new Date();
+        dateObj.setHours(+timeParts[0]);
+        dateObj.setMinutes(+timeParts[1]);
+        dateObj.setSeconds(+timeParts[2]);
+
+        // Format the time according to the specified format
+        let hours = dateObj.getHours();
+        const minutes = dateObj.getMinutes();
+        const seconds = dateObj.getSeconds();
+
+        let rtn = '';
+        if (timeFormat === '12h') {
+            const period = hours >= 12 ? 'PM' : 'AM';
+            hours = hours % 12;
+            hours = hours ? hours : 12; // the hour '0' should be '12' in 12h format
+            rtn = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds} ${period}`;
+        } else if (timeFormat === '24h') {
+            rtn = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        } else {
+            throw new Error(`Invalid time format: ${timeFormat}`);
+        }
+
+        return rtn;
+    }
+
+
+
+    // TODO
+    setTime(): void {
+
     }
 
     /**
