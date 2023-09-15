45621230908020242bb639577f7a794918632caa
Lachlan <lachlancresswell@gmail.com>
2023-09-15 10:41:07 +1000

    refactor (chart)

--- client/src/Pages/PageChart.tsx
+++ client/src/Pages/PageChart.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { InfluxDB, FluxTableMetaData } from '@influxdata/influxdb-client-browser';
+import { InfluxDB } from '@influxdata/influxdb-client-browser';
 import ReactApexChart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
 import { mockPollRange, org, token } from '../Hooks/neighbourDataContext';
 import TuneIcon from '@mui/icons-material/Tune';
 import '../Styles/PageChart.css'
-import { useConfigDataContext } from '../Hooks/configContext';
 import { usePhaseColors } from '../Hooks/usePhaseColors';
 
 export interface RowRtn {
@@ -22,6 +21,33 @@ export interface RowRtn {
     type: string,
 }
 
+const legendStartState = [
+    { voltage: true, current: true },
+    { voltage: true, current: true },
+    { voltage: true, current: true }
+]
+
+/**
+ * Tests a URL to ensure it is accessible
+ * @param host url to test
+ * @returns resolves on success
+ */
+const testUrl = (host: string) => new Promise((res, rej) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+
+            if (!(this.status >= 200 && this.status <= 299)) {
+                return rej(`Cannot reach ${host}`);
+            }
+
+            return res('Success');
+        }
+    };
+    xhttp.open("GET", host, true)
+    xhttp.send();
+});
+
 
 export interface Phase {
     voltage: { y: number | string | null, x: any }[]
@@ -30,24 +56,26 @@ export interface Phase {
 
 interface Props { }
 
-const MyComponent: React.FC<Props> = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [data, setData] = useState<Phase[]>([]);
-    const [options, setOptions] = useState<ApexOptions>({})
-    const { configData } = useConfigDataContext();
+const collectData = async (hostUrl: string, databaseUrl: string) => {
+    await testUrl(hostUrl);
+    await testUrl(databaseUrl)
 
-    const { l1Color, l2Color, l3Color } = usePhaseColors(); //{ l1Color: '#ff0000', l2Color: '#00ff00', l3Color: '#00f0ff' }
 
-    const url = window.location.protocol + '//' + window.location.host + '/influx'
-    useEffect(() => {
-        const queryApi = new InfluxDB({ url, token }).getQueryApi(org);
+    const db = new InfluxDB({ url: databaseUrl, token, timeout: 5000 });
+    const queryApi = db.getQueryApi(org);
 
-        setData(mockPollRange());
-        setIsLoading(false);
+    let phases: Phase[] = [{
+        voltage: [],
+        amperage: [],
+    }, {
+        voltage: [],
+        amperage: [],
+    }, {
+        voltage: [],
+        amperage: [],
+    }];
 
-        return;
-
-        const query = `
+    const query = `
         from(bucket: "modbus")
         |> range(start: -10s)
         |> filter(fn: (r) => r["_measurement"] == "cpu")
@@ -56,187 +84,174 @@ const MyComponent: React.FC<Props> = () => {
         |> yield(name: "mean")
     `;
 
-        let phases: Phase[] = [{
-            voltage: [],
-            amperage: [],
-        }, {
-            voltage: [],
-            amperage: [],
-        }, {
-            voltage: [],
-            amperage: [],
-        }];
+    queryApi.collectRows<RowRtn>(query).then((result) => {
 
-        queryApi.collectRows<RowRtn>(query).then((result) => {
-
-            result!.forEach((row) => {
-                switch (row._field) {
-                    case 'usage_user':
-                        phases[0].amperage.push({ y: row._value, x: new Date(row._time) })
-                        phases[1].amperage.push({ y: row._value, x: new Date(row._time) })
-                        phases[2].amperage.push({ y: row._value, x: new Date(row._time) })
-                        break;
-                    case 'usage_system':
-                        phases[0].voltage.push({ y: row._value, x: row._time })
-                        phases[1].voltage.push({ y: row._value, x: row._time })
-                        phases[2].voltage.push({ y: row._value, x: row._time })
-                        break;
-                }
-            });
-
-            setData(phases);
-            setIsLoading(false);
+        result!.forEach((row) => {
+            switch (row._field) {
+                case 'usage_user':
+                    phases[0].amperage.push({ y: row._value, x: new Date(row._time) })
+                    phases[1].amperage.push({ y: row._value, x: new Date(row._time) })
+                    phases[2].amperage.push({ y: row._value, x: new Date(row._time) })
+                    break;
+                case 'usage_system':
+                    phases[0].voltage.push({ y: row._value, x: row._time })
+                    phases[1].voltage.push({ y: row._value, x: row._time })
+                    phases[2].voltage.push({ y: row._value, x: row._time })
+                    break;
+            }
         });
-    }, []);
+
+    });
+
+    return phases;
+}
+
+const initialOptions = (l1Color: string, l2Color: string, l3Color: string) => {
+    const yaxisV = {
+        showAlways: true,
+        seriesName: 'Voltage',
+        min: 180,
+        max: 250,
+        tickAmount: 4,
+        labels: {
+            style: {
+                fontSize: '1em',
+            },
+            formatter: (value: any) => (value >= 200) ? value.toFixed(value % 1 != 0 ? 1 : 0) + 'V' : undefined
+        },
+    }
+
+    const yaxisC = {
+        showAlways: true,
+        seriesName: 'Current',
+        opposite: true,
+        labels: {
+            style: {
+                fontSize: '1em',
+            },
+            formatter: (value: any) => (value <= 10) ? value.toFixed(value % 1 != 0 ? 1 : 0) + 'A' : undefined
+        },
+        min: 0,
+        max: 20,
+        tickAmount: 3,
+        forceNiceScale: true
+    }
+
+    return ({
+        chart: {
+            id: "mychart",
+            foreColor: 'white',
+            type: "area",
+            animations: {
+                enabled: false
+            },
+            toolbar: {
+                autoSelected: 'pan',
+                show: false
+            }
+        }, noData: {
+            text: 'Loading...'
+        },
+        colors: [l1Color, l1Color, l2Color, l2Color, l3Color, l3Color],
+        fill: {
+        },
+        legend: {
+            show: false
+        },
+        markers: {
+            size: 0,
+            colors: ['#7777ff'],
+            showNullDataPoints: false,
+        },
+        xaxis: {
+            labels: {
+                formatter: function (value: any, timestamp: any) {
+                    const d = new Date(value as any).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+                    return d
+                },
+                style: {
+                    fontSize: '1em' // specify the font size here
+                },
+            },
+            tickAmount: 4,
+        },
+        yaxis: [
+            // Phase 1
+            yaxisV, yaxisC,
+            // Phase 2 - hide axis labels to prevent duplicates
+            { ...yaxisV, ...{ show: false } }, { ...yaxisC, ...{ show: false } },
+            // Phase 3 - hide axis labels to prevent duplicates
+            { ...yaxisV, ...{ show: false } }, { ...yaxisC, ...{ show: false } }
+        ]
+    } as ApexOptions)
+}
+
+const MyComponent: React.FC<Props> = () => {
+    const [plotData, setPlotData] = useState<Phase[]>([]);
+
+    // Load phase colours
+    const { l1Color, l2Color, l3Color } = usePhaseColors();
+
+    // Plot configuration options
+    const [options, setOptions] = useState<ApexOptions>(initialOptions(l1Color, l2Color, l3Color))
 
     useEffect(() => {
-        if (!isLoading) {
-            const options: ApexOptions = {
-                chart: {
-                    id: "mychart",
-                    foreColor: 'white',
-                    // group: 'social',
-                    type: "area",
-                    animations: {
-                        enabled: false
-                    },
-                    toolbar: {
-                        autoSelected: 'pan',
-                        show: false
-                    }
-                }, noData: {
-                    text: 'Loading...'
-                },
-                colors: [l1Color, l1Color, l2Color, l2Color, l3Color, l3Color],
-                fill: {
-                },
-                legend: {
-                    show: false
-                },
-                markers: {
-                    size: 0,
-                    colors: ['#7777ff'],
-                    showNullDataPoints: false,
-                },
-                xaxis: {
-                    labels: {
-                        formatter: function (value, timestamp) {
-                            const d = new Date(value as any).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-                            return d
-                        },
-                        style: {
-                            fontSize: '1em' // specify the font size here
-                        },
-                    },
-                    tickAmount: 3
-                },
-                yaxis: [
-                    {
-                        seriesName: 'Voltage',
-                        min: 230,
-                        max: 250,
-                        tickAmount: 3,
-                        decimalsInFloat: 0,
-                        labels: {
-                            style: {
-                                fontSize: '1.5em' // specify the font size here
-                            },
-                            formatter: (value) => value.toFixed() + 'V'
-                        },
-                    }, {
-                        seriesName: 'Current',
-                        opposite: true,
-                        labels: {
-                            style: {
-                                fontSize: '1.5em', // specify the font size here
-                            },
-                            formatter: (value) => value.toFixed() + 'A'
-                        },
-                        min: 0,
-                        max: 10,
-                        tickAmount: 3,
-                        decimalsInFloat: 0,
-                    }, {
-                        seriesName: 'Voltage',
-                        show: false
-                    }, {
-                        seriesName: 'Current',
-                        show: false
-                    }, {
-                        seriesName: 'Voltage',
-                        show: false
-                    }, {
-                        seriesName: 'Current',
-                        show: false
-                    }
-                ]
-            };
-            setOptions(options)
-        }
-    }, [isLoading, data]);
+        const hostUrl = window.location.protocol + '//' + window.location.host;
+        const databaseUrl = hostUrl + '/influx'
+        collectData(hostUrl, databaseUrl).then((res) => {
+            setPlotData(res)
+        }, (text) => {
+            setPlotData(mockPollRange())
+            // setOptions({ ...options, noData: { text } })
+        })
+    }, []);
 
-    let [legendView, setLegendView] = useState<{ [index: string]: boolean }[]>([
-        { voltage: true, current: true },
-        { voltage: true, current: true },
-        { voltage: true, current: true }
-    ]);
-    const handleHideClick = (phaseIndex: 1 | 2 | 3, category: "Voltage" | "Current") => {
-        ApexCharts.exec("mychart", "hideSeries", [`L${phaseIndex} ${category}`]);
+    let [legendView, setLegendView] = useState<{ [index: string]: boolean }[]>(legendStartState);
+
+    const toggleLegendElement = (phaseIndex: 1 | 2 | 3, category: "Voltage" | "Current") => {
+        const hide = !!legendView[phaseIndex - 1][category.toLowerCase()];
+
+        ApexCharts.exec("mychart", hide ? "hideSeries" : "showSeries", [`L${phaseIndex} ${category}`]);
         legendView[phaseIndex - 1][category.toLowerCase()] = false;
         legendView = [...legendView];
         setLegendView(legendView);
-    };
-    const handleShowClick = (phaseIndex: 1 | 2 | 3, category: "Voltage" | "Current") => {
-        ApexCharts.exec("mychart", "showSeries", [`L${phaseIndex} ${category}`]);
-        legendView[phaseIndex - 1][category.toLowerCase()] = true;
-        legendView = [...legendView];
-        setLegendView(legendView);
-    };
-    const toggleLegendElement = (phaseIndex: 1 | 2 | 3, category: "Voltage" | "Current") => {
-        if (legendView[phaseIndex - 1][category.toLowerCase()]) {
-            handleHideClick(phaseIndex, category);
-        } else {
-            handleShowClick(phaseIndex, category);
-        }
     }
 
+    const configuredData = configureData(plotData);
     return (
         <>
-            :
-            <>
-                <div style={{ height: "80%", width: "100%" }}>
-                    <ReactApexChart type="line" options={options} series={configureData(data)} height={"100%"} />
+            <div style={{ height: "72%", width: "100%" }}>
+                <ReactApexChart type="line" options={options} series={configuredData} height={"100%"} />
+            </div>
+            <div className={`chart-buttons`}>
+                <div className='chart-button'>
+                    +
                 </div>
-                <div className={`chart-buttons`}>
-                    <div className='chart-button'>
-                        +
-                    </div>
-                    <div className={`chart-button l1 ${legendView[1 - 1].voltage ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(1, "Voltage")}>
-                        V
-                    </div >
-                    <div className={`chart-button l1 ${legendView[1 - 1].current ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(1, "Current")}>
-                        A
-                    </div>
-                    <div className={`chart-button l2 ${legendView[2 - 1].voltage ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(2, "Voltage")}>
-                        V
-                    </div>
-                    <div className={`chart-button l2 ${legendView[2 - 1].current ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(2, "Current")}>
-                        A
-                    </div>
-                    <div className={`chart-button l3 ${legendView[3 - 1].voltage ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(3, "Voltage")}>
-                        V
-                    </div>
-                    <div className={`chart-button l3 ${legendView[3 - 1].current ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(3, "Current")}>
-                        A
-                    </div>
-                    <div className='chart-button'>
-                        -
-                    </div>
-                    <div className='chart-button'>
-                        <TuneIcon />
-                    </div>
+                <div className={`chart-button l1 ${legendView[1 - 1].voltage ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(1, "Voltage")}>
+                    V
+                </div >
+                <div className={`chart-button l1 ${legendView[1 - 1].current ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(1, "Current")}>
+                    A
                 </div>
-            </>
+                <div className={`chart-button l2 ${legendView[2 - 1].voltage ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(2, "Voltage")}>
+                    V
+                </div>
+                <div className={`chart-button l2 ${legendView[2 - 1].current ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(2, "Current")}>
+                    A
+                </div>
+                <div className={`chart-button l3 ${legendView[3 - 1].voltage ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(3, "Voltage")}>
+                    V
+                </div>
+                <div className={`chart-button l3 ${legendView[3 - 1].current ? '' : 'strikethrough'}`} onClick={() => toggleLegendElement(3, "Current")}>
+                    A
+                </div>
+                <div className='chart-button'>
+                    -
+                </div>
+                <div className='chart-button'>
+                    <TuneIcon />
+                </div>
+            </div>
         </>
     );
 };
