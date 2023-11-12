726b782f5be43f4b5363d1bc3e55619590131d7c
Lachlan <lachlancresswell@gmail.com>
2023-11-12 14:59:09 +0000

    refactor (client): move client to seperate repo

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM mcr.microsoft.com/devcontainers/typescript-node:1-20-bullseye
-RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install git
-RUN addgroup --gid 20 macos
-RUN adduser --uid 501 --gid 20 macos
--- client/src/Hooks/usePhaseColors.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-import { LocaleOptions } from '../../../Types';
-
-export function usePhaseColors(pluginConfig: LocaleOptions) {
-    useEffect(() => {
-        document.body.dataset.locale = pluginConfig?.locale.value as string || 'au';
-    }, [pluginConfig.locale.value])
-}
